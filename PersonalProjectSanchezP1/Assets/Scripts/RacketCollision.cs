6d069bf4e477830544e75090788f01c87a2b8e4a
Ashtonval <tennisjedi20xx@gmail.com>
2023-05-21 10:05:03 -0700

    Did my own code for racket collision
    Still in the process for racket collision, need to make score and destroy game object

--- PersonalProjectSanchezP1/Assets/Scripts/RacketCollision.cs
+++ PersonalProjectSanchezP1/Assets/Scripts/RacketCollision.cs
@@ -4,37 +4,15 @@ using UnityEngine;
 
 public class RacketCollision : MonoBehaviour
 {
-    private float thrust = 80.0f;
     public Rigidbody Ballrb;
-    public float Score;
-    public int ScoreToAdd;
-    private GameManager gameManager;
-    float ballXRange = 15;
-    
-    
-    void Start ()
+    public float thrust = 80.0f;
+
+    private void OnCollisionEnter(Collision collisionInfo)
     {
-        Ballrb = GetComponent<Rigidbody>();
-
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-
-
-    }
-
-    public void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "Player")
-        { 
-
-            Ballrb.velocity = Vector3.zero;
-            Ballrb.AddForce(Random.Range(ballXRange, -ballXRange) ,Random.Range(0, -10) , thrust, ForceMode.Impulse);
-            
-
-            gameManager.UpdateScore(1);
-        }
-        if (collision.gameObject.name == "GameBound")
+        if(collisionInfo.collider.tag == "Ball")
         {
-            gameManager.GameOver();
+            Ballrb = GetComponent<Rigidbody>();
+            Ballrb.AddForce(0, 0, thrust, ForceMode.Impulse);
         }
     }
 }
